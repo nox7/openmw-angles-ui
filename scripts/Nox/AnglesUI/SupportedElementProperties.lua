733ee49004a29006a449e5ef2bdb0fc4843e890a
Cole Green <17110935+nox7@users.noreply.github.com>
2026-03-29 10:19:47 -0500

    Modify support element properties

---
+++ scripts/Nox/AnglesUI/SupportedElementProperties.lua
@@ -0,0 +1,25 @@
+-- The allowed properties/attributes when parsing each supported element.
+return {
+  ["mw-root"] = {
+    "Layer";
+    "Width";
+    "Height";
+    "RelativeWidth";
+    "RelativeHeight";
+    "RelativeX";
+    "RelativeY";
+    "X";
+    "Y";
+    "AnchorX";
+    "AnchorY";
+  };
+  ["mw-window"] = {
+    
+  };
+  ["mw-flex"] = {
+    "Padding";
+  };
+  ["mw-text"] = {
+    "TextColor";
+  };
+}
