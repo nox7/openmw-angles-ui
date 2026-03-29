f3eca04c2cd75d4ad523fcbbba6b6ffe95d4076f
Cole Green <17110935+nox7@users.noreply.github.com>
2026-03-29 10:19:34 -0500

    Add Renderer.FromFile(vfs, components)

--- scripts/Nox/AnglesUI/Renderer/Renderer.lua
+++ scripts/Nox/AnglesUI/Renderer/Renderer.lua
@@ -1,4 +1,5 @@
 local AcceptedEngineTagNames = require("scripts.Nox.AnglesUI.AcceptedEngineTagNames")
+local SupportedElementProperties = require("scripts.Nox.AnglesUI.SupportedElementProperties")
 local Lexer = require("scripts.Nox.AnglesUI.Lexer.Lexer")
 local Evaluator = require("scripts.Nox.AnglesUI.Evaluator.Evaluator")
 local Context = require("scripts.Nox.AnglesUI.Evaluator.Context")
@@ -8,6 +9,7 @@ local Core = require("openmw.core")
 local MWUI = require('openmw.interfaces').MWUI
 local Node = require("scripts.Nox.AnglesUI.Lexer.Nodes.Node")
 local TableUtils = require("scripts.Nox.Utils.TableUtils")
+local VFS = require('openmw.vfs')
 
 -- The user's menu transparency setting from the Settings::gui().mTransparencyAlpha value
 local menuTransparencyAlphaValue = UI._getMenuTransparency()
@@ -26,7 +28,19 @@ Renderer.IsValidEngineTag = function(tagName)
   return false
 end
 
-function Renderer.new(source, userComponents)
+-- Creates a render from a virtual file path
+function Renderer.FromFile(vfsPath, userComponents)
+  local file = VFS.open(vfsPath)
+  if (file == nil) then
+    error("Could not find file at path: " .. vfsPath)
+  end
+
+  local source = file:read("*a")
+  file:close()
+  return Renderer.New(source, userComponents)
+end
+
+function Renderer.New(source, userComponents)
   local self = setmetatable({}, Renderer)
   self.source = source
   self.userComponents = userComponents or {}
@@ -42,14 +56,16 @@ function Renderer:Render(userContext)
   local context = Context.new(userContext)
   local rootNode = evaluator:evaluate(ast, context)
 
+  -- TODO
+  -- Run an Effect() on all userContexts that are signals, so that when they update we can destroy the uiElement and re-render
+  -- a new one with updated values.
+
   if (#rootNode.children > 0) then
     local firstNode = rootNode.children[1]
     if (firstNode.type == Node.TYPE_ENGINE_COMPONENT) then
       if (firstNode.tagName == "mw-root") then
         local uiElement = self:RenderChildren(firstNode, nil)
         uiElement:update()
-        print("Rendered " .. tostring(uiElement))
-        TableUtils.PrintTable(uiElement.layout)
         return uiElement
       else
         error("The first component of a rendered template must be a mw-root element. You provided a " .. firstNode.tagName);
@@ -98,14 +114,46 @@ function Renderer:GetEngineUIElement(node)
   if (node.type == Node.TYPE_ENGINE_COMPONENT) then
     local tagName = node.tagName
     if (Renderer.IsValidEngineTag(tagName)) then
+      local allProperties = self:ParseAcceptedProperties(node)
       if (tagName == "mw-root") then
+        local layer = allProperties["layer"]
+        local width = allProperties["width"]
+        local height = allProperties["height"]
+        local relativeWidth = allProperties["relativewidth"]
+        local relativeHeight = allProperties["relativeheight"]
+        local relativeX = allProperties["relativex"]
+        local relativeY = allProperties["relativey"]
+        local xPos = allProperties["x"]
+        local yPos = allProperties["y"]
+        
+        if (layer == nil) then
+          error("mw-root elements must have a 'Layer' attribute.")
+        end
+
+        if ( (width == nil or height == nil) and (relativeWidth == nil or relativeHeight == nil)) then
+          error("mw-root elements must have 'Width' and 'Height' attributes or 'RelativeWidth' and 'RelativeHeight' attributes.")
+        end
+
+        local props = {}
+
+        if (width ~= nil and height ~= nil) then
+          props.size = Util.vector2(width, height)
+        end
+        
+        if (relativeWidth ~= nil and relativeHeight ~= nil) then
+          props.relativeSize = Util.vector2(relativeWidth, relativeHeight)
+        end
+
+        if (xPos ~= nil and yPos ~= nil) then
+          props.position = Util.vector2(xPos, yPos)
+        end
+        if (relativeX ~= nil and relativeY ~= nil) then
+          props.relativePosition = Util.vector2(relativeX, relativeY)
+        end
+
         return {
-          layer = node:getAttribute("layer"),
-          props = {
-            size = Util.vector2(800, 400),
-            relativePosition = Util.vector2(0.5, 0),
-            anchor = Util.vector2(0,0),
-          }
+          layer = layer,
+          props = props
         }
       elseif (tagName == "mw-window") then
         return {
@@ -135,14 +183,59 @@ function Renderer:GetEngineUIElement(node)
         -- If there is padding, then parse it and apply it to the relativeSize, size, and position accordingly
         local padding = node:getAttribute("padding")
         local parsedPadding = self:ParsePadding(padding or "0")
+        -- We check for parsedPadding and not padding
+        local parsedPadding = allProperties["parsedpadding"]
+        local relativeWidth = allProperties["relativewidth"]
+        local relativeHeight = allProperties["relativeheight"]
+        local width = allProperties["width"]
+        local height = allProperties["height"]
+        local relativeX = allProperties["relativex"]
+        local relativeY = allProperties["relativey"]
+        local xPos = allProperties["x"]
+        local yPos = allProperties["y"]
+
+        if ( (width == nil or height == nil) and (relativeWidth == nil or relativeHeight == nil)) then
+          -- In this case, default-set the relativeWidth and relativeHeight to 1. Common users will assume
+          -- that a flex element expands to fit its container from web standards.
+          relativeWidth = 1;
+          relativeHeight = 1;
+        end
+
+        local props = {}
+
+        if (relativeWidth ~= nil and relativeHeight ~= nil) then
+          props.relativeSize = Util.vector2(relativeWidth, relativeHeight)
+        end
+
+        if (width ~= nil and height ~= nil) then
+          props.size = Util.vector2(width, height)
+          if (parsedPadding ~= nil) then
+            props.size = props.size - Util.vector2(parsedPadding.Left + parsedPadding.Right, parsedPadding.Top + parsedPadding.Bottom)
+          end
+        else
+          if (parsedPadding ~= nil) then
+            props.size = Util.vector2(-parsedPadding.Left - parsedPadding.Right, -parsedPadding.Top - parsedPadding.Bottom)
+          end
+        end
+
+        if (relativeX ~= nil and relativeY ~= nil) then
+          props.relativePosition = Util.vector2(relativeX, relativeY)
+        end
+
+        if (xPos ~= nil and yPos ~= nil) then
+          props.position = Util.vector2(xPos, yPos)
+          if (parsedPadding ~= nil) then
+            props.position = props.position + Util.vector2(parsedPadding.Left, parsedPadding.Top)
+          end
+        else
+          if (parsedPadding ~= nil) then
+            props.position = Util.vector2(parsedPadding.Left, parsedPadding.Top)
+          end
+        end
 
         return {
           type = UI.TYPE.Flex,
-          props = {
-            relativeSize = Util.vector2(1,1),
-            size = Util.vector2(0,0) - Util.vector2(parsedPadding.Left + parsedPadding.Right, parsedPadding.Top + parsedPadding.Bottom),
-            position = Util.vector2(parsedPadding.Left, parsedPadding.Top)
-          }
+          props = props
         }
       elseif (tagName == "mw-text") then
         local textNodesText = ""
@@ -181,6 +274,40 @@ function Renderer:GetEngineUIElement(node)
   else
     error("Cannot render node of type " .. node.type)
   end
+end
+
+-- Gets all accepted properties for the node element
+-- If necessary, parses the properties in correct order so that
+-- their integrity is maintained (e.g. padding needs to be parsed before we can calculate size and position)
+function Renderer:ParseAcceptedProperties(node)
+  local properties = {}
+  if (node.tagName == "mw-root") then
+    for _, propertyName in pairs(SupportedElementProperties["mw-root"]) do
+      local loweredPropertyName = string.lower(propertyName)
+      local attributeValue = node:getAttribute(loweredPropertyName)
+      if (attributeValue ~= nil) then
+        properties[loweredPropertyName] = attributeValue
+      end
+    end
+  elseif (node.tagName == "mw-window") then
+
+  elseif (node.tagName == "mw-flex") then
+    -- We need to parse padding before we can calculate size and position, so we do that first
+    local padding = node:getAttribute("padding")
+    if (padding ~= nil) then
+      properties["padding"] = padding
+      local parsedPadding = self:ParsePadding(padding)
+      properties["parsedpadding"] = parsedPadding
+    end
+    for _, propertyName in pairs(SupportedElementProperties["mw-flex"]) do
+      local loweredPropertyName = string.lower(propertyName)
+      local attributeValue = node:getAttribute(loweredPropertyName)
+      if (attributeValue ~= nil) then
+        properties[loweredPropertyName] = attributeValue
+      end
+    end
+  end
+  return properties
 end
 
 -- Parses a string that can contain between 1 and 4 numbers
